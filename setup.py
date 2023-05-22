54f052e5688b42ad0b1c74ecd19614f00a880963
SpuelMett <bejoh55@gmail.com>
2023-05-22 19:10:47 +0200

    remove python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -32,8 +32,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Object Brokering',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     keywords='python boinc xml api rpc gui science computing',
